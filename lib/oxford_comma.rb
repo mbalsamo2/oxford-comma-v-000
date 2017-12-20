812ed16cccc98995e5c385257f4e0d4102ffe58d
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-20 01:41:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     "#{array[0]} and #{array[1]}"
   else
     last_guy = array.pop
-    array.join(", ") + " and " + last_guy
+    array.join(", ") + ", and " + last_guy
 
   end
 end
