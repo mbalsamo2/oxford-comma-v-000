9cfeafac3255d98d4e4c484258a294c0d5cb9fab
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-20 01:04:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,15 @@
 def oxford_comma(array)
   # array.join(", ")
-
-  if array.length == 2
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
     "#{array[0]} and #{array[1]}"
 # elsif array.length == 3
 #   "#{array[0]}, #{array[1]}, and #{array[2]}"
   else
     array.insert(-2, "and")
     array.join(", ")
-
+    
   end
 
 end
